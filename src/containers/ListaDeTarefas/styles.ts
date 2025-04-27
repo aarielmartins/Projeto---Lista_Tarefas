94a578bbf2c88447fe47787c06cf9c5f7603636c
ariel <you@example.com>
2025-04-27 13:56:30 -0300

    iniciando o estado dos botões

--- src/containers/ListaDeTarefas/styles.ts
+++ src/containers/ListaDeTarefas/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const Container = styled.main`
-  padding: 0 40px;
+  padding: 40px;
   height: 100vh;
   // TODO CONTEÚDO QUE ULTRAPASSAR O VH DE 80 VAI SER ROLADO DENTRO
   // DO CONTAINER NO EIXO Y (VERTICAL)
